6a71267a217de46b28ee82eec8c414647b8264c0
Antonina\Tosia <fenskolo@gmailcom>
2018-05-21 21:55:28 +0200

    test

---
+++ WindowsFormsApp1/WindowsFormsApp1/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public class Class1
+    {
+        private SqlConnection con;
+        private Form2 a;
+        public Class1(Form2 f)
+        {
+            a = f;
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "ANTONINA";
+            builder.InitialCatalog = "testowy";
+            builder.IntegratedSecurity = true;
+            con = new SqlConnection(builder.ConnectionString);
+            DataSet ds = new DataSet();
+            con.InfoMessage += new SqlInfoMessageEventHandler(Connection_InfoMessage);
+            Task task = Task.Factory.StartNew(() =>
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = con;
+                    cmd.CommandText = "dbo.test";
+                    cmd.CommandTimeout = 120;
+
+
+                    cmd.Connection.Open();
+
+                    using (SqlDataAdapter sa = new SqlDataAdapter(cmd))
+                    {
+                        Task.Delay(300).Wait();
+                        sa.Fill(ds);
+                        cmd.Connection.Close();
+                        a.Invoke((MethodInvoker)delegate ()
+                        {
+                            a.Close();
+                        });
+                    }
+                }
+            }
+            );
+
+        }
+
+        private void Connection_InfoMessage(object sender, SqlInfoMessageEventArgs e)
+        {
+            a.label1.Invoke((MethodInvoker)delegate ()
+            {
+                a.label1.Text = e.Message;
+            });
+        }
+
+        //s. = "server=(local);user id=ab;" +
+        //     "password= a!Pass113;initial catalog=AdventureWorks";
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Form2 f = new Form2();
+            Task task = Task.Factory.StartNew(() =>
+            {
+                f.ShowDialog();
+            });
+            Class1 cl = new Class1(f);
+        }
+    }
+}
